fc62ddc9b9bdd6338109b97ec8c864a955e34202
Septiana Harun <55664528+septian-id@users.noreply.github.com>
2024-10-25 16:41:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
     api_url             : 'https://desktop.niceradius.com/v4',
-    user_id             : 'YG23RJC4LS4914U0',
-    router_id           : 'W9MZ2M7IEEZROLS8',
+    user_id             : '',
+    router_id           : '',
     contact             : 'https://wa.me/628XXXXXXXXXX'
 }
