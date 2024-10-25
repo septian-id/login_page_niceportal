a10407510d90613cecaad0601e7b7b0684dfb5d0
Septiana Harun <55664528+septian-id@users.noreply.github.com>
2024-10-25 09:40:15 +0000

    update config

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
 var config = {
-clear_session       : true,
-api_url             : 'https://desktop.niceradius.com/api',
-api_key             : '',
-router_id           : '',
-merchant_code       : '',
-merchant_api_key    : '',
-fonnte_token        : '',
-contact             : 'https://wa.me/6281234567890',
-currency            : 'Rp',
-color               : 17
+    api_url             : 'https://desktop.niceradius.com/v4',
+    user_id             : 'YG23RJC4LS4914U0',
+    router_id           : 'W9MZ2M7IEEZROLS8',
+    contact             : 'https://wa.me/628XXXXXXXXXX'
 }
