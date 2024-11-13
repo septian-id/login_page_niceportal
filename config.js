1057fb73fc81adda71b45c0943885b03b901b9c4
Septiana Harun <55664528+septian-id@users.noreply.github.com>
2024-11-13 02:29:32 +0000

    update qrcode scanner

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ var config = {
     api_url             : 'https://desktop.niceradius.com/v4',
     user_id             : '',
     router_id           : '',
-    contact             : 'https://wa.me/628XXXXXXXXXX'
+    scanner_url         : 'https://qrcodescan.in/'
 }
